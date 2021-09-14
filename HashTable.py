123c98ddc46716c55c33f1ea0efbf6e342ae8404
Antonio Barros-Limon <tbl@Antonios-MacBook-Air.local>
2021-09-14 00:15:37 -0700

    first commit

---
+++ HashTable.py
@@ -0,0 +1,16 @@
+class HashTable:
+    def __init__(self, size = 16):
+        self.map = [None] * size
+
+    def __get_hash(self, key):
+        pass
+
+    def addToHash(self, key, value):
+        pass
+
+    def getHash(self, key):
+        pass
+
+    def delete(self, key):
+        pass
+
