4548af1e14bdbd8c23de6d385f9135bf530ba0c4
tonybarroslimon <tonybarroslimon@outlook.com>
2021-09-18 23:49:20 -0700

    Created and implemented the package class

--- Package.py
+++ Package.py
@@ -1,2 +1,22 @@
+import csv
+from HashTable import HashTable
+
+
 class Package:
-    pass
+
+    # Creates an instance of a hash table
+    package_hash = HashTable()
+
+    def __init__(self):
+        with open('data/package_file.csv') as package_file:
+            self.package_data = csv.reader(package_file, delimiter=',')
+
+            for row_data in self.package_data:
+                self.key = row_data[0]
+                Package.package_hash.set_item(self.key, [self.key, row_data[1], row_data[2], row_data[3], row_data[4],
+                                                         row_data[5], row_data[6], row_data[7]])
+
+
+
+
+
