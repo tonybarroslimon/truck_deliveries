3f1a694d1fbeca831898afaa84948e8502d06e5e
tonybarroslimon <tonybarroslimon@outlook.com>
2021-09-18 23:18:01 -0700

    Finished the Truck class. Using inheritance for the different trucks given they will have different packages.

--- Truck.py
+++ Truck.py
@@ -3,9 +3,13 @@ import HashTable
 
 class Truck:
 
+    truck_speed = 18 / 60
+
     def __init__(self):
         self.truck_distance = 0.0
         self.truck_destinations = []
+        self.truck_packages = []
+        self.truck_priority = []
         self.truck_priority_destinations = []
         self.truck_pos = 0
         self.truck_curr_pos = 0.0
@@ -13,6 +17,54 @@ class Truck:
         self.truck_returned = False
         self.truck_departed = False
         self.package_deliveries = [None] * 41
+
+    def get_truck_distance(self):
+        return self.truck_distance
+
+    def set_truck_distance(self, distance):
+        self.truck_distance = distance
+
+    def get_truck_destinations(self):
+        return self.truck_destinations
+
+    def set_truck_destinations(self, address_id):
+        self.truck_destinations.append(address_id)
+
+    def get_truck_packages(self):
+        return self.truck_packages
+
+    def set_truck_packages(self, package_id):
+        self.truck_packages.append(package_id)
+
+    def get_truck_priority(self):
+        return self.truck_priority
+
+    def set_truck_priority(self, package_id):
+        self.truck_priority.append(package_id)
+
+    def get_truck_priority_destinations(self):
+        return self.truck_priority_destinations
+
+    def set_truck_priority_destinations(self, address_id):
+        self.truck_priority_destinations.append(address_id)
+
+    def get_truck_pos(self):
+        return self.truck_pos
+
+    def set_truck_pos(self, package_id):
+        self.truck_pos.append(package_id)
+
+    def get_truck_curr_pos(self):
+        return self.truck_curr_pos
+
+    def set_truck_curr_pos(self, package_id):
+        self.truck_curr_pos.append(package_id)
+
+    def get_truck_goal(self):
+        return self.truck_goal
+
+    def set_truck_goal(self, package_id):
+        self.truck_goal.append(package_id)
 
 
 class Truck1(Truck):
