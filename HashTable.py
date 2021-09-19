f20f8395e760417f1d0c707c03755bd47aaefb69
tonybarroslimon <tonybarroslimon@outlook.com>
2021-09-18 23:04:44 -0700

    Updated the HashTable to remove collisions given that the table will never be bigger than 16 which won't actually cause any collisions. This will make using this class easier to use in the bigger project.

--- HashTable.py
+++ HashTable.py
@@ -1,36 +1,30 @@
 class HashTable:
-    """"Creates a hash table of size 16. This hash table handles collisions using chaining."""
+    """"Creates a hash table of size 16."""
     def __init__(self):
+        # Maximum number of packages that can be placed in a truck.
         self.PACKAGES = 16
 
         # Creates an array of arrays of the size of PACKAGES.
         self.array = [[] for i in range(self.PACKAGES)]
 
+    # Creates the hash instance using the asci values of the characters in the key value
     def get_hash(self, key):
         hash_value = 0
         for char in key:
             hash_value += ord(char)
         return hash_value % self.PACKAGES
 
+    # This method allows an item to be added to the hash table
     def set_item(self, key, value):
         hash_val = self.get_hash(key)
-        found = False
-        for index, element in enumerate(self.array[hash_val]):
-            if element[0] == key and len(element) == 2:
-                self.array[hash_val][index] = (key,value)
-                found = True
-                break
-        if not found:
-            self.array[hash_val].append((key,value))
+        self.array[hash_val] = value
 
+    # This method lets the user query the hash table
     def get_item(self, key):
         hash_val = self.get_hash(key)
-        for element in self.array[hash_val]:
-            if element[0] == key:
-                return element[1]
+        return self.array[hash_val]
 
+    # This method allows a user to delete an item from teh hash table
     def delete_item(self, key):
         hash_val = self.get_hash(key)
-        for index, key_value in enumerate(self.array[hash_val]):
-            if key_value[0] == key:
-                del self.array[hash_val][index]
+        self.array[hash_val] = None
