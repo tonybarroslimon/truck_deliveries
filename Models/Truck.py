b21443a224853c83573ae8ca750b2e970025e9e7
tonybarroslimon <tonybarroslimon@outlook.com>
2021-10-05 22:11:42 -0700

    Simplified the Truck class and removed the 3rd truck subclass.

--- Models/Truck.py
+++ Models/Truck.py
@@ -1,86 +1,32 @@
 class Truck:
 
     def __init__(self):
-        self.truck_distance = 0.0
-        self.truck_destinations = []
-        self.truck_packages = []
-        self.truck_priority = []
-        self.truck_priority_destinations = []
-        self.truck_pos = 0
-        self.truck_curr_pos = 0.0
-        self.truck_goal = 0.0
-        self.truck_returned = False
-        self.truck_departed = False
-        self.truck_next = None
-
-    def get_truck_distance(self):
-        return self.truck_distance
-
-    def set_truck_distance(self, distance):
-        self.truck_distance = distance
-
-    def get_truck_destinations(self):
-        return self.truck_destinations
-
-    def set_truck_destinations(self, address_id):
-        self.truck_destinations.append(address_id)
-
-    def get_truck_packages(self):
-        return self.truck_packages
-
-    def set_truck_packages(self, package_id):
-        self.truck_packages.append(package_id)
-
-    def get_truck_priority(self):
-        return self.truck_priority
-
-    def set_truck_priority(self, package_id):
-        self.truck_priority.append(package_id)
-
-    def get_truck_priority_destinations(self):
-        return self.truck_priority_destinations
-
-    def set_truck_priority_destinations(self, address_id):
-        self.truck_priority_destinations.append(address_id)
-
-    def get_truck_pos(self):
-        return self.truck_pos
-
-    def set_truck_pos(self, package_id):
-        self.truck_pos.append(package_id)
-
-    def get_truck_curr_pos(self):
-        return self.truck_curr_pos
-
-    def set_truck_curr_pos(self, package_id):
-        self.truck_curr_pos.append(package_id)
-
-    def get_truck_goal(self):
-        return self.truck_goal
-
-    def set_truck_goal(self, package_id):
-        self.truck_goal.append(package_id)
+        self.distance = 0.0
+        self.destinations = []
+        self.packages = []
+        self.priority = []
+        self.priority_destinations = []
+        self.pos = 0
+        self.curr_pos = 0.0
+        self.goal = 0.0
+        self.returned = False
+        self.departed = False
+        self.next = None
 
 
 class Truck1(Truck):
 
     def __init__(self):
         Truck.__init__(self)
-        self.truck_packages = [13, 14, 15, 16, 29, 30, 31, 34, 37, 40, 1, 4, 7, 39, 8, 32]
-        self.truck_priority = [13, 14, 15, 16, 29, 30, 31, 34, 37, 40]
+        self.early_packages = [13, 14, 15, 16, 29, 30, 31, 34, 37, 40, 1, 4, 7, 39, 8, 32]
+        self.early_priority = [13, 14, 15, 16, 29, 30, 31, 34, 37, 40]
+        self.late_packages = [9, 19, 27, 28, 33, 35, 2, 5]
+        self.late_priority = []
 
 
 class Truck2(Truck):
 
     def __init__(self):
         Truck.__init__(self)
-        self.truck_packages = [3, 18, 36, 38, 6, 10, 11, 12, 17, 20, 21, 22, 23, 24, 25, 26]
-        self.truck_priority = [6, 20, 25]
-
-
-class Truck3(Truck):
-
-    def __init__(self):
-        Truck.__init__(self)
-        self.truck_packages = [9, 19, 27, 28, 33, 35, 2, 5]
-        self.truck_priority = []
+        self.packages = [3, 18, 36, 38, 6, 10, 11, 12, 17, 20, 21, 22, 23, 24, 25, 26]
+        self.priority = [6, 20, 25]
