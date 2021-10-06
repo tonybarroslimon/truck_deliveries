e15822e59087436acc762d8a6ee31ac0221a2863
tonybarroslimon <tonybarroslimon@outlook.com>
2021-10-05 22:04:56 -0700

    This is going to be completely updated. I am commiting this just in case I need to go back.

--- app.py
+++ app.py
@@ -23,12 +23,9 @@ class App:
         self.package_deliveries = [None for i in range(41)]
         # Blank list to hold removed packages
         self.removal_list = []
-        # Keeps track of the next stop for a truck
-        self.truck_next = None
         self.simulation_end = None
         self.current_time = None
-        # adds an item to the package hash table
-
+        self.packages.set_item("9", ['9', '300 State St', 'Salt Lake City', 'UT', '84103', 'EOD', '2', 'Wrong address listed'])
 
     def reset(self):
         # deletes the objects created when App is constructed.
@@ -43,38 +40,40 @@ class App:
         return float(self.truck1.truck_distance + self.truck2.truck_distance + self.truck3.truck_distance)
 
     # Method to build the delivery locations for each truck
-    def load_packages(self, truck):
+    @staticmethod
+    def load_packages(truck):
         for index in truck.truck_packages:
-            address = Package.package_hash.get_item(str(index))[1]
-            address_id = Distance.address_hash.get_item(address)[0]
-
-            if address_id not in truck.truck_packages:
+            address = Package().package_hash.get_item(str(index))[1]
+            address_id = Distance().address_hash.get_item(address)[0]
+            if address_id not in truck.truck_destinations:
                 truck.truck_destinations.append(address_id)
                 if index in truck.truck_priority:
                     truck.truck_priority_destinations.append(address_id)
 
     def unload_packages(self, truck_packages, truck_priority, truck_pos):
         for index in truck_packages:
-            address = App.packages.get_item(str(index))[1]
-            address_id = Distance.address_hash.get_item(address)[0]
+            address = self.packages.get_item(str(index))[1]
+            address_id = Distance().address_hash.get_item(address)[0]
 
             if address_id == str(truck_pos):
                 self.removal_list.append(index)
                 self.package_deliveries[index] = [index, self.current_time.time(), "Delivered"]
 
         for item in self.removal_list:
-            truck_packages.remove(item)
+            if item in truck_packages:
+                truck_packages.remove(item)
             if item in truck_priority:
                 truck_priority.remove(item)
 
     # Sets the initial next destination for a truck
     # If there is a priority package on the truck, then the method will choose the closest priority package
-    def set_initial_next_destination(self, truck):
+    @staticmethod
+    def set_initial_next_destination(truck):
         if len(truck.truck_priority_destinations) > 0:
-            self.truck_next = Distance.next_closest_stop(truck.truck_pos, truck.truck_priority_destinations)
-        elif len(self.truck_destinations) > 0:
-            self.truck_next = self.distance.next_closest_stop(truck.truck_pos, truck.truck_destinations)
-        truck.truck_goal = float(self.truck_next[1])
+            truck.truck_next = Distance().next_closest_stop(truck.truck_pos, truck.truck_priority_destinations)
+        else:
+            truck.truck_next = Distance().next_closest_stop(truck.truck_pos, truck.truck_destinations)
+        truck.truck_goal = float(truck.truck_next[1])
 
     # method updates the current position and the distance to the next location
     def update_pos_and_distance(self, truck):
@@ -87,20 +86,21 @@ class App:
     def set_truck_destinations(self, truck):
         if truck.truck_curr_pos >= truck.truck_goal:
             truck.truck_curr_pos -= truck.truck_goal
-            truck.truck_pos = self.truck_next[0]
+            truck.truck_pos = truck.truck_next[0]
             self.unload_packages(truck.truck_packages, truck.truck_priority, truck.truck_pos)
-            truck.truck_destinations.remove(self.truck_next[0])
+            truck.truck_destinations.remove(truck.truck_next[0])
             if len(truck.truck_priority_destinations) > 0:
-                truck.truck_priority_destinations.remove(self.truck_next[0])
-                self.truck_next = Distance.next_closest_stop(truck.truck_pos, truck.truck_priority_destinations)
+                truck.truck_priority_destinations.remove(truck.truck_next[0])
+                truck.truck_next = Distance().next_closest_stop(truck.truck_pos, truck.truck_priority_destinations)
             elif len(truck.truck_destinations) > 0:
-                self.truck_next = Distance.next_closest_stop(truck.truck_pos, truck.truck_destinations)
-            elif len(truck.truck_destinations) < 1 and self.truck_next[0] != 0:
+                truck.truck_next = Distance().next_closest_stop(truck.truck_pos, truck.truck_destinations)
+            elif len(truck.truck_destinations) < 1 and truck.truck_next[0] != 0:
                 truck.truck_destinations.append(0)
-                self.truck_next = (0, Distance.distance_home(truck.truck_pos))
-            truck.truck_goal = float(self.truck_next[1])
+                truck.truck_next = (0, Distance().distance_home(truck.truck_pos))
+            truck.truck_goal = float(truck.truck_next[1])
 
-    def update_truck_returned(self, truck):
+    @staticmethod
+    def update_truck_returned(truck):
         if len(truck.truck_destinations) < 1:
             truck.truck_returned = True
 
@@ -117,10 +117,10 @@ class App:
 
         self.truck1.truck_departed = True
 
-        while ((len(self.truck1.truck_destinations) >= 1 and
-                len(self.truck2.truck_destinations) >= 1 and
-                len(self.truck3.truck_destinations) >= 1) or
-               (self.simulation_end >= self.current_time.time())):
+        while not ((len(self.truck1.truck_destinations) < 1 and
+                len(self.truck2.truck_destinations) < 1 and
+                len(self.truck3.truck_destinations) < 1) or
+               (self.current_time.time() >= self.simulation_end)):
 
             # Increments the current time by one minute each loop
             self.current_time += datetime.timedelta(minutes=1)
@@ -129,8 +129,8 @@ class App:
             if self.current_time.time() == datetime.datetime.strptime('09:05:00', '%H:%M:%S').time():
                 self.truck2.truck_departed = True
             if self.truck1.truck_returned and self.current_time.time() >= datetime.datetime.strptime('10:20:00', '%H:%M:%S').time():
-                self.packages.set_item('9', ['9', '300 State St', 'Salt Lake City', 'UT', '84103', 'EOD', '2',
-                                             'Wrong address listed'])
+                self.packages.delete_item("9")
+                self.packages.set_item("9", ["9", "410 S State St", "Salt Lake City", "UT", "84111", "EOD", "2", "None"])
                 self.truck3.truck_departed = True
 
             self.update_pos_and_distance(self.truck1)
@@ -149,7 +149,7 @@ class App:
 
         if hash_id is None:
             for i in range(1,41):
-                self.package.append(Package.package_hash.get_item(str(i)))
+                self.package.append(Package().package_hash.get_item(str(i)))
 
                 if self.package_deliveries[i] is not None:
                     self.package.append(str(self.package_deliveries[i]))
@@ -178,7 +178,7 @@ class App:
                 self.package.pop()
 
         else:
-            self.package.append(Package.package_hash.get_item(str(hash_id)))
+            self.package.append(Package().package_hash.get_item(str(hash_id)))
 
             if self.package_deliveries[hash_id] is not None:
                 self.package.append(str(self.package_deliveries[hash_id]))
