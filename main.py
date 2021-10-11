d6c7453b25cdcd9adecae265f9f6f306f9b6b2be
tonybarroslimon <tonybarroslimon@outlook.com>
2021-10-11 03:23:49 -0700

    minor update

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ import sys
 
 print("Name: Tony Barros-Limon")
 print("Student ID: 000950459")
+print("Python Version: 3.10.0rc2")
+print("IDE: PyCharm 2021.2.1 (Professional Edition)")
 
 simulation = App()
 simulation.run_simulation("18:00")
-print(f"All packages were delivered in {simulation.get_total_distance()} miles.")
+print(f"\nAll packages were delivered in {simulation.get_total_distance()} miles.")
 
 while True:
     print("\n1. Package Search")
